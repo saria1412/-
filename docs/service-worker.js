9c18f30c9625b529a770cb610314b86da903f3eb
saria1412 <saria.kholani@gmail.com>
2026-08-04 20:23:01 +0300

    Add Paper Mill service worker

---
+++ docs/service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'paper-mill-shell-v3-pages';
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './logo.svg'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  if (event.request.method !== 'GET' || url.origin !== self.location.origin) return;
+  const networkFirst = event.request.mode === 'navigate' || url.pathname.endsWith('index.html') || url.pathname.endsWith('service-worker.js');
+  if (networkFirst) {
+    event.respondWith(fetch(event.request).then((response) => {
+      if (response.ok) caches.open(CACHE_NAME).then((cache) => cache.put(event.request.mode === 'navigate' ? './index.html' : event.request, response.clone()));
+      return response;
+    }).catch(() => caches.match(event.request.mode === 'navigate' ? './index.html' : event.request)));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+    if (response.ok) caches.open(CACHE_NAME).then((cache) => cache.put(event.request, response.clone()));
+    return response;
+  })));
+});
